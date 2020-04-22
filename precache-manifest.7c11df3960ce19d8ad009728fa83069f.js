4791fc3360d9d52088bab0de5f6074765f204feb
atharva <atharva.lord10@gmail.com>
2020-04-22 15:43:15 +0530

    Updates

---
+++ precache-manifest.7c11df3960ce19d8ad009728fa83069f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10426a7daf3406a30f86b01ff10e8431",
+    "url": "/Monsters_Inc/index.html"
+  },
+  {
+    "revision": "26c6a1a1d05e6319882a",
+    "url": "/Monsters_Inc/static/css/main.a4b0ba9f.chunk.css"
+  },
+  {
+    "revision": "bd4da6ab1d9e405c24fb",
+    "url": "/Monsters_Inc/static/js/2.e552c0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters_Inc/static/js/2.e552c0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c6a1a1d05e6319882a",
+    "url": "/Monsters_Inc/static/js/main.1c10f91a.chunk.js"
+  },
+  {
+    "revision": "3b0dd260559383a3f332",
+    "url": "/Monsters_Inc/static/js/runtime-main.bc13a769.js"
+  }
+]);
